77907c70d7a1026a8ca9a60c0d3feb3e1cf7fa1a
beunchoi <byungeun0228@gmail.com>
2024-12-20 01:45:22 +0900

    @PostConstruct, @PreDestroy

--- src/test/java/com/practice/core/lifecycle/NetworkClient.java
+++ src/test/java/com/practice/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package com.practice.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
 
   private String url;
@@ -25,13 +28,13 @@ public class NetworkClient {
   public void disconnect() {
     System.out.println("close: " + url);
   }
-
+  @PostConstruct
   public void init() {
     System.out.println("NetworkClient.init");
     connect();
     call("초기화 연결 메시지");
   }
-
+  @PreDestroy
   public void close() {
     System.out.println("NetworkClient.close");
     disconnect();
